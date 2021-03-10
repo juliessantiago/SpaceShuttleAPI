48a917dca23d7fc8188ab1421167b6948ae45e31
Julie <Santiago.sjulie@gmail.com>
2021-03-10 17:16:56 -0300

    Adiciona módulo de conexão usando Mongoose

--- source/index.js
+++ source/index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const App = express(); 
+
+
+App.use(bodyParser.json())
+App.use(bodyParser.urlencoded({extended : false}))
+
+App.get('/', (req, res) => {
+    res.send('Ok')
+})
+App.listen(2000, () => {
+    console.log('Servidor on')
+})
